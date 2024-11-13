3f069d0028916c2108bf35665f11940c8c95bf0d
luccacastro <luccazutin@gmail.com>
2024-11-13 12:29:34 +0000

    add env vars

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/tasks';
+const API_URL =` ${import.meta.env.VITE_API_URL}/api/tasks`;
 
 export const getTasks = async () => {
   return axios.get(API_URL);
